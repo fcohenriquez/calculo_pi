57964f3b8033f816025641790fb6ece7d8ac5fc5
fcohenriquez <32282112+fcohenriquez@users.noreply.github.com>
2021-09-28 12:40:02 -0300

    Update calc_pi.py

--- calc_pi.py
+++ calc_pi.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 def pi_est(n):
     '''
@@ -12,14 +13,20 @@ n = 1000
 numExperimentos = 100000
 
 # Calcula Pi con map y una funcion
-
+start = time.time()
 listaValoresPi=list(map(pi_est, [n]*numExperimentos))
+end = time.time()
+print(end - start)
 
 print('media= '+str(np.mean(listaValoresPi)))
 print ('mediana= '+str(np.median(listaValoresPi)))
 #plt.plot(listaValoresPi)
 
 # Calcula pi con map y lambda
+start = time.time()
 listaValoresPi1=list(map(lambda x : 4*sum((np.square(np.random.uniform(0, 1, x).tolist()) + np.square(np.random.uniform(0, 1, x).tolist()))<=1)/x,  [n]*numExperimentos))
+end = time.time()
+print(end - start)
+
 print('media= '+str(np.mean(listaValoresPi1)))
 print ('mediana= '+str(np.median(listaValoresPi1)))
